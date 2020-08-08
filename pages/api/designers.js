35b4f7f35311a6201362a1b4ee019ff011688ab2
Andrés Richero <arichero@gmail.com>
2020-08-08 16:36:56 -0300

    Update designers.js

--- pages/api/designers.js
+++ pages/api/designers.js
@@ -1,11 +1,9 @@
 import drive from "drive-db";
-
 export default async (req, res) => {
   const db = await drive("1bGjq4zGnt4DMtmF5l6Jo3xBaVh1HaVdhSU9M995EW_k");
   let sanitizeResult = db.filter(
     (item) => item.name != "" && item.show == "Yes"
   );
-
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(sanitizeResult));
