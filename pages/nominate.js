588532be1294857a18254c2ad39de8058b3ed0db
Andrés Richero <arichero@gmail.com>
2020-08-08 18:17:18 -0300

    Update nominate.js

--- pages/nominate.js
+++ pages/nominate.js
@@ -19,7 +19,7 @@ export default function Nominate({ designers }) {
           height="100%"
           frameBorder="0"
           allow="camera; microphone; autoplay; encrypted-media;"
-          src="https://form.typeform.com/to/bbrPUqMe"
+          src="https://ixdamontevideo.typeform.com/to/DKSAOgSG"
         ></iframe>{" "}
         <script
           type="text/javascript"
