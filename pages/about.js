9849675cb3a3a40fc7da0a6164738e9bb2f23be1
Andrés Richero <arichero@gmail.com>
2020-08-08 19:27:26 -0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -56,12 +56,8 @@ export default function Home({ designers }) {
           <motion.span variants={item}>The goal</motion.span>{" "}
           <motion.span variants={item}>is to inspire new</motion.span>{" "}
           <motion.span variants={item}>designers to diversify</motion.span>{" "}
-          <motion.span variants={item}>
-            their references, experienced designers
-          </motion.span>{" "}
-          <motion.span variants={item}>
-            to diversify their network, and companies to diversify their teams.
-          </motion.span>
+          <motion.span variants={item}>their references, experienced designers</motion.span>{" "}
+          <motion.span variants={item}>to diversify their network, and companies to diversify their teams.</motion.span>
         </p>
       </motion.div>
 
