1d83ebeb15fc4fc725aa0cfb07fad3f0adaf99d1
Andrés Richero <arichero@gmail.com>
2020-08-19 19:12:38 -0300

    Update Metatags.js

--- components/Metatags.js
+++ components/Metatags.js
@@ -4,19 +4,19 @@ export default function MetaTags() {
       <meta name="viewport" content="width=device-width,initial-scale=1" />
 
       {/* <!-- Primary Meta Tags --> */}
-      <meta name="title" content="Uruguayans Who Design" />
+      <meta name="title" content="Uruguayans Who Design. A Uruguayan designers repository." />
       <meta
         name="description"
-        content="A repository to celebrate the work of talented Uruguayan designers and showcase it to the world. Explore, discover and inspire."
+        content="We celebrate the work of talented Uruguayan designers and showcase it to the world. Explore, discover and inspire."
       />
 
       {/* <!-- Open Graph / Facebook --/> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="http://uruguayanswho.design" />
-      <meta property="og:title" content="Uruguayans Who Design" />
+      <meta property="og:title" content="Uruguayans Who Design. A Uruguayan designers repository." />
       <meta name="description"
         property="og:description"
-        content="A repository to celebrate the work of talented Uruguayan designers and showcase it to the world. Explore, discover and inspire."
+        content="We celebrate the work of talented Uruguayan designers and showcase it to the world. Explore, discover and inspire."
       />
       <meta name="image"
         property="og:image"
@@ -26,10 +26,10 @@ export default function MetaTags() {
       {/* <!-- Twitter --/> */}
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="http://uruguayanswho.design" />
-      <meta property="twitter:title" content="Uruguayans Who Design" />
+      <meta property="twitter:title" content="Uruguayans Who Design. A Uruguayan designers repository." />
       <meta
         property="twitter:description"
-        content="A repository to celebrate the work of talented Uruguayan designers and showcase it to the world. Explore, discover and inspire."
+        content="We celebrate the work of talented Uruguayan designers and showcase it to the world. Explore, discover and inspire."
       />
       <meta
         property="twitter:image"
