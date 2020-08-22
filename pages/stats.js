66a5d5ef96717ffd4008d003a5ac0964159512c7
Andrés Richero <arichero@gmail.com>
2020-08-22 17:46:55 -0300

    Create stats.js

---
+++ pages/stats.js
@@ -0,0 +1,177 @@
+import Head from "next/head";
+import { useState, useEffect, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Nav from "../components/Nav.js";
+import Title from "../components/Title.js";
+import MetaTags from "../components/Metatags.js";
+import Analytics from "../components/Analytics.js";
+import FilterSVG from "../components/Icons/FilterSVG.js";
+
+export async function getStaticProps() {
+  const origin =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : "https://uruguayanswho.design/";
+
+  const res = await fetch(`${origin}/api/designers`);
+  const designers = await res.json();
+
+  return {
+    props: {
+      designers,
+    },
+  };
+}
+
+export default function Stats({ designers }) {
+  const [isReady, setIsReady] = useState(false);
+  const [designersList, setDesignersList] = useState(null);
+
+  useEffect(() => {
+    setDesignersList(designers.sort((a, b) => a.order > b.order));
+  }, []);
+
+  return (
+    <div
+      className="container"
+      style={{
+        overflow: isReady ? "hidden" : "auto",
+      }}
+    >
+      <Head>
+        <title>Uruguayans Who Design. A Uruguayan designers repository.</title>
+        <link id="favicon" rel="alternate icon" href="/favicon.ico" />
+        <MetaTags />
+      </Head>
+
+      {!isReady ? (
+        <Content
+          designers={designersList}
+        />
+      ) : null}
+
+      <style global jsx>{`
+        html,
+        body {
+          overflow: ${filterIsOpen ? "hidden" : "auto"};
+        }
+      `}</style>
+    </div>
+  );
+}
+
+function Content({ designers, className, onClick }) {
+  const tableHeaderRef = useRef();
+
+  useEffect(() => {
+    const header = tableHeaderRef.current;
+    const sticky = header.getBoundingClientRect().top + 40;
+    const scrollCallBack = window.addEventListener("scroll", () => {
+      if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", scrollCallBack);
+    };
+  }, []);
+
+  return (
+    <div className={className} onClick={onClick}>
+      <Nav />
+
+      <Title className="title m0 p0" text="Uruguayans*who&nbsp;design" />
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+      >
+        <table className="large tableContent" cellSpacing="0">
+          <thead id="tableHeader" ref={tableHeaderRef}>
+            <tr>
+              <td>Data</td>
+              <td
+                className="thsize-loc dn">Data</td>
+            </tr>
+          </thead>
+          {designers != null ? (
+            <tbody>
+              {designers.map((d, i) => (
+                <tr key={`${d.name}-${i}`}>
+                  <td><a href={d.link} target="_blank" rel="noopener">{d.data}</a></td>
+                  <td className="thsize-loc dn"><a href={d.link}>{d.count}</a></td>
+                </tr>
+              ))}
+            </tbody>
+          ) : null}
+        </table>
+      </motion.div>
+      <style jsx>{`
+        .tableContent {
+          padding-top: 18vh;
+        }
+
+        .filterTable {
+          cursor: pointer;
+        }
+
+        thead {
+          height: 2.2rem;
+        }
+
+        .thsize-loc {
+          width: 15%;
+        }
+
+        .thsize-aux {
+          width: 25%;
+        }
+
+        .thsize-link {
+          width: 2rem;
+          text-align: right;
+        }
+
+        @media (max-width: 480px) {
+          .thsize-aux {
+            width: 30%;
+          }
+        }
+
+        tbody a {
+          width: 100%;
+          padding-bottom: 0.6em;
+          padding-top: 0.6em;
+          color: inherit;
+          display: inline-block;
+        }
+
+        table tbody td {
+          padding-top: 0;
+          padding-bottom: 0;
+        }
+
+      `}</style>
+
+      <Analytics />
+    </div>
+  );
+}
+
+function shuffle(array) {
+  var m = array.length,
+    temp,
+    i;
+
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    temp = array[m];
+    array[m] = array[i];
+    array[i] = temp;
+  }
+
+  return array;
+}
