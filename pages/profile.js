c01797d35ea49b0975a10bf70d4d8d183c23d90b
Andrés Richero <arichero@gmail.com>
2020-08-22 16:33:01 -0300

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -8,30 +8,46 @@ import Analytics from "../components/Analytics.js";
 
 // TODO: Need to fetch `posts` (by calling some API endpoint)
 //       before this page can be pre-rendered.
-function Profile({ designers }) {
+function Profile({ posts }) {
   return (
     <ul>
-      {designers.map((d) => (
-        <li>{d.name}</li>
+      {posts.map((post) => (
+        <li>{post.title}</li>
       ))}
     </ul>
   )
 }
 
+export async function getStaticPaths() {
+  const origin =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : "https://uruguayanswho.design/";
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://.../posts')
+  const posts = await res.json()
+
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post) => `/posts/${post.slug}`)
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
+}
+
 // This also gets called at build time
 export async function getStaticProps({ params }) {
+  const origin =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : "https://uruguayanswho.design/";
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`https://.../${params.slug}`)
-  const designers = await res.json()
+  const res = await fetch(`https://.../posts/${params.slug}`)
+  const post = await res.json()
 
   // Pass post data to the page via props
-  return {
-    props: {
-      designers,
-    },
-  };
+  return { props: { post } }
 }
 
 export default Profile
-
