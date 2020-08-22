4dfb048ff547aa691ec0d7c6e8a48dad45306c79
Andrés Richero <arichero@gmail.com>
2020-08-22 16:52:49 -0300

    Update profile.js

--- pages/profile.js
+++ pages/profile.js
@@ -18,19 +18,18 @@ function Profile({ posts }) {
   )
 }
 
-// This also gets called at build time
-export async function getServerSideProps({ params }) {
-  const origin =
-    process.env.NODE_ENV !== "production"
-      ? "http://localhost:3000"
-      : "https://uruguayanswho.design/";
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`https://.../profile/${params.slug}`)
+// This function gets called at build time
+export async function getStaticPaths() {
+  // Call an external API endpoint to get posts
+  const res = await fetch('https://.../profile')
   const posts = await res.json()
 
-  // Pass post data to the page via props
-  return { props: { post } }
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post) => `/profile/${post.slug}`)
+
+  // We'll pre-render only these paths at build time.
+  // { fallback: false } means other routes should 404.
+  return { paths, fallback: false }
 }
 
 export default Profile
