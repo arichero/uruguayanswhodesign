90232a9e4a90194f6f2709775528d429018ac416
Andrés Richero <arichero@gmail.com>
2020-08-23 09:39:05 -0300

    Update [slug].js

--- pages/profile/[slug].js
+++ pages/profile/[slug].js
@@ -1,3 +1,11 @@
+import Head from "next/head";
+import { useState, useEffect, useRef } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Nav from "../components/Nav.js";
+import Filter from "../components/Filter.js";
+import Title from "../components/Title.js";
+import MetaTags from "../components/Metatags.js";
+import Analytics from "../components/Analytics.js";
 import { useRouter } from 'next/router'
 
 const Profile = () => {
