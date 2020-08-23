49f2d6cdd705b5fb4b30813bc3a9cfab076521b6
Andrés Richero <arichero@gmail.com>
2020-08-23 10:49:52 -0300

    Update [slug].js

--- pages/profile/[slug].js
+++ pages/profile/[slug].js
@@ -1,10 +1,43 @@
+// pages/posts/[id].js
 import { useRouter } from 'next/router'
 
-const Post = () => {
+function Post({ post }) {
   const router = useRouter()
-  const { slug } = router.query
 
-  return <p>Post: {slug}</p>
+  // If the page is not yet generated, this will be displayed
+  // initially until getStaticProps() finishes running
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  // Render post...
+}
+
+// This function gets called at build time
+export async function getStaticPaths() {
+  return {
+    // Only `/posts/1` and `/posts/2` are generated at build time
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    // Enable statically generating additional pages
+    // For example: `/posts/3`
+    fallback: true,
+  }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  // params contains the post `id`.
+  // If the route is like /posts/1, then params.id is 1
+  const res = await fetch(`https://.../posts/${params.id}`)
+  const post = await res.json()
+
+  // Pass post data to the page via props
+  return {
+    props: { post },
+    // Re-generate the post at most once per second
+    // if a request comes in
+    revalidate: 1,
+  }
 }
 
 export default Post
