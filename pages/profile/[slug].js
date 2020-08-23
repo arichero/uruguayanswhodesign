e70935f2de444ae32ad83489169f5a61cc7c553d
Andrés Richero <arichero@gmail.com>
2020-08-23 13:32:04 -0300

    Update [slug].js

--- pages/profile/[slug].js
+++ pages/profile/[slug].js
@@ -1,15 +1,22 @@
-const Profile = ({ slug }) => (
+const Profile = ({ slug })
+// This gets called on every request
+const origin =
+    process.env.NODE_ENV !== "production"
+      ? "http://localhost:3000"
+      : "https://uruguayanswho.design/";
+
+  const res = await fetch(`${origin}/api/designers`);
+  const designers = await res.json();
+
+  // Pass data to the page via props
+  return { props: { designers } }
+}
+
+export default function Profile({ designers }) {
   <div>
-    <h1>{name}</h1>
-    <p>Welcome to our product page for {slug}!</p>
+    <h1>{d.name}</h1>
+    <p>Welcome to our product page for {d.slug}!</p>
   </div>
 )
-
-Profile.getInitialProps = async ({ query }) => {
-  return { 
-    slug: query.slug,
-    name: query.name
-  }
-}
 
 export default Profile
