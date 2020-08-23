a5ba5b9fd24d8e8fda4d84d069d84ba782ebde98
Andrés Richero <arichero@gmail.com>
2020-08-23 11:43:00 -0300

    Update [slug].js

--- pages/profile/[slug].js
+++ pages/profile/[slug].js
@@ -28,7 +28,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`https://.../profile/${params.slug}`)
+  const res = await fetch(`https://uruguayanswho.design/profile/${params.slug}`)
   const post = await res.json()
 
   // Pass post data to the page via props
