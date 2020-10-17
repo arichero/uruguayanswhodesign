a2a5dc2dcb7c7798955c6ae13ae982789dc10ab1
Andrés Richero <arichero@gmail.com>
2020-10-17 17:36:07 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -220,7 +220,7 @@ function Content({ designers, handleOpenFilter, className, onClick }) {
             <tbody>
               {designers.map((d, i) => (
                 <tr key={`${d.name}-${i}`}>
-                  <td><a href={d.link} target="_blank" rel="noopener" title="`{d.name}` | `{d.expertise}`">{d.name}</a></td>
+                  <td><a href={d.link} target="_blank" rel="noopener" title="`${d.name}` | `${d.expertise}`">{d.name}</a></td>
                   <td className="thsize-loc dn"><a href={d.link}>{d.location}</a></td>
                   <td className="thsize-aux"><a href={d.link}>{d.expertise}</a></td>
                   <td className="thsize-link"><a href={d.link}>→</a></td>
