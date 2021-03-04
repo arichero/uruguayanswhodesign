49600c9d72853a7800fcbc7c5ba4105e38444973
Andrés Richero <arichero@gmail.com>
2021-03-03 23:18:06 -0300

    Update EmailSVG.js

--- components/Icons/EmailSVG.js
+++ components/Icons/EmailSVG.js
@@ -1,11 +1,11 @@
 export default function EmailSVG() {
   return (
     <svg
-      style={{ marginLeft: "0.4rem"; display: inline-block; }}
+      style={{ marginLeft: "0.4rem", display: "inline-block" }}
       width="32"
       height="32"
       fill="none"
-      viewBox="0 0 17 10"
+      viewBox="0 0 32 32"
     >
     <path d="M4 7H28V24C28 24.2652 27.8946 24.5196 27.7071 24.7071C27.5196 24.8946 27.2652 25 27 25H5C4.73478 25 4.48043 24.8946 4.29289 24.7071C4.10536 24.5196 4 24.2652 4 24V7Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
